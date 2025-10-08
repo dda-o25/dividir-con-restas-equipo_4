c4381e59a1a5f601cd2045f0d2b72b8de50f51e7
angelicalbarradiazz-stack <angelicalbarradiazz@gmail.com>
2025-10-08 12:48:17 -0600

    cambios totales

--- dividir_con_restas.py
+++ dividir_con_restas.py
@@ -1,25 +1,18 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
+#Entradas
+#Se pide un número dividendo y un número divisor
+dividendo = int(input("Introduzca el dividendo:"))
+divisor = int(input("Introduzca el divisor"))
+cociente_división=0 #Establecemos que el valor inicial del conciente de la división es cero
+residuo_división = dividendo #Así establecemos que la cantidad del dividendo se convierte en residui
 
-# Declaraciones
-#CONSTANTE = valor
-
-
-# Entradas
-#entrada = int(input(""))
-dividendo = int(input("dividendo:"))
-divisor = int(input("divisor"))
-cociente=0
-residuo = dividendo
 # Proceso
    
-while residuo >= divisor:
-    residuo = residuo - divisor
-    cociente= cociente + 1
-
-print("cociente",cociente)
-print("Residuo:", residuo)
+while residuo_división >= divisor: #Con el while indicamos que esto pasará sólamente mientras 
+#el resiudo sea mayor o igual al divisor 
+ 
+    residuo_división = residuo_división - divisor # con esta lpinea se efectúa la resta
+    cociente_división= cociente_división + 1 #Y si fué posible la resta, entonces el conciente aumenta una vez más
 
 # Salidas
-#print(salida)
+print("El cociente",cociente_división) #para que nos muestre el cociente
+print("El residuo:", residuo_división) #Para que nos muestre el residuio
