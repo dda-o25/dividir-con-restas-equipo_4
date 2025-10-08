ba39526594c71923b2e0676f444c04fc2e282895
angelicalbarradiazz-stack <angelicalbarradiazz@gmail.com>
2025-10-08 10:16:40 -0600

    aaa

--- dividir_con_restas.py
+++ dividir_con_restas.py
@@ -3,13 +3,23 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
+#CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
-
+#entrada = int(input(""))
+dividendo = int(input("dividendo:"))
+divisor = int(input("divisor"))
+cociente=0
+residuo = dividendo
 # Proceso
+   
+while residuo >= divisor:
+    residuo = residuo - divisor
+    cociente= cociente + 1
 
+print("cociente",cociente)
+print("Residuo:", residuo)
 
 # Salidas
-print(salida)
+#print(salida)
